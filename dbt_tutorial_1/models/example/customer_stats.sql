d559549407279266b5484059ae07377908ac3c27
Istvan Toth <tott.isti@gmail.com>
2024-11-12 20:51:00 +0100

    models

---
+++ dbt_tutorial_1/models/example/customer_stats.sql
@@ -0,0 +1,39 @@
+with w_customers as (
+    select
+        id,
+        first_name,
+        last_name
+    from customers
+),
+
+w_orders as (
+    select
+        id,
+        customer_id,
+        order_date
+    from orders
+),
+
+w_customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(id) as number_of_orders
+    from w_orders
+    group by customer_id
+),
+
+final as (
+    select
+        w_customers.id as customer_id,
+        w_customers.first_name,
+        w_customers.last_name,
+        w_customer_orders.first_order_date,
+        w_customer_orders.most_recent_order_date,
+        coalesce(w_customer_orders.number_of_orders, 0) as number_of_orders
+    from w_customers
+    left join w_customer_orders on w_customers.id = w_customer_orders.customer_id
+)
+
+select * from final
